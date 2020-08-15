073b95ccb2ed906946fe98db236848d16a544d68
Andrew <andrewyen64@gmail.com>
2020-08-15 11:38:50 -0700

    Add artist table model

---
+++ models/artist.js
@@ -0,0 +1,26 @@
+module.exports = function(sequelize, DataTypes) {
+  var Artist = sequelize.define("Artist", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    song: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+  return Artist;
+};
