f7d220e3f4983aaf9b9e9858373876349dd50d27
Andrew <andrewyen64@gmail.com>
2020-08-15 12:59:45 -0700

    Edit get route and change typo in delete route

--- routes/artist-api-routes.js
+++ routes/artist-api-routes.js
@@ -27,14 +27,14 @@ module.exports = function(app) {
 
   // POST route for saving a new artist's info
   app.post("/api/artists", function(req, res) {
-    db.Post.create(req.body).then(function(dbArtist) {
+    db.Artist.create(req.body).then(function(dbArtist) {
       res.json(dbArtist);
     });
   });
 
   // DELETE route for deleting saved artist info
   app.delete("/api/artists/:id", function(req, res) {
-    db.Post.destroy({
+    db.Artist.destroy({
       where: {
         id: req.params.id
       }
