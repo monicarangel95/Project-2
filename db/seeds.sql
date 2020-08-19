0e387f616f74c1641d197ad5f0c61e35e14fbcdd
Andrew <andrewyen64@gmail.com>
2020-08-18 18:55:03 -0700

    Remove unused seeds.sql

--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO artists (name, genre, song) VALUES ('The Killers', 'indie', 'Mr. Brightside');
-INSERT INTO artists (name, genre, song) VALUES ('Migos', 'Hip-Hop', 'Bad and Boujee (Feat. Lil Uzi Vert)');
-INSERT INTO artists (name, genre, song) VALUES ('Elton John', 'pop', 'Your Song');
