38a6cf4eb30b957fbdec3285c9c46f81c8cdde49
Monica Rangel <monicarangel95@github.com>
2020-08-18 20:07:50 -0700

    edits to the script files

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,93 +1,115 @@
+//Materialize JQuery
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+})
+
+//API STUFF?
+
+
+//GLOBAL VARIABLES
 var events = 0;
 
-$("#search").click(function () {
-    let artistSearch = $("#artist").val();
-    //console.log(artistSearch)
-    fmSearch(artistSearch);
-    ticketSearch(artistSearch);
-});
+//GETTING THE SEARCHED ARTIST FROM
+//THE SEARCH BAR
+$("#search-button").click(function(){
+  let artist = $("#search-artist").val();
+  // console.log(artist)
+  //This Hides the Description of our website
+  //so what the user searches appears at the top
+  $("#photos").addClass("hide")
+  //Shows Artist & Event Info Once The Aritst is Searched
+  $("#events-searched").removeClass("hide")
+  $("#artist").removeClass("hide")
+  lastFM(artist)
+  BIT(artist)
+  createCard()
+})
+//------------------------------------------
 
-function fmSearch(artistSearch){
-    $("#artistName").text(artistSearch);
-    artistSearch = encodeURIComponent(artistSearch.trim())
-    console.log(artistSearch)
-
-    //Searching for Top Songs
-    var queryURL1 = "https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + artistSearch + "&api_key=c480f7f0a05a2b8e0d17968d01265fbe&format=json"
-
-    $.ajax({
-        url: queryURL1,
-        method: "GET",
-      }).then(function(response) {
-        for(i = 0; i < 3; i++){
-          $("#songTitle" + i).text(response.toptracks.track[i].name)
-        }
-      });
-
-    //Searching for Artist Info
-    var queryURL2 = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artistSearch + "&api_key=c480f7f0a05a2b8e0d17968d01265fbe&format=json"
-
-    $.ajax({
-      url: queryURL2,
+function lastFM(artist){
+  //GETS ARTIST SUMMARY
+  artist = encodeURIComponent(artist.trim())
+  var queryURL1 = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artist + "&api_key=4228a7dc9f02d04d2a06295ef5d3ac49&format=json"
+  $.ajax({
+      url: queryURL1,
       method: "GET",
-    }).then(function(response) {
-      $(".subtitle").text(response.artist.tags.tag[0].name);
+  }).then(function(response){
+      console.log(response)
+      $("#artist-name").text(response.artist.name)
+      
+      //TO GET ARTIST SUMAMRY AND CUT OFF THE URL AT THE END
+      // console.log(response.artist.bio.summary)
+      var summary = response.artist.bio.summary.split('<');
+      $("#summary").text(summary[0])
 
-      console.log(response);
+      //TO GET THE GENRE OF THE ARTIST
+      $("#genre").text("Genre: " + response.artist.tags.tag[0].name)
+  })
 
-    });
+  var queryURL2 = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + artist + "&api_key=4228a7dc9f02d04d2a06295ef5d3ac49&format=json"
+  $.ajax({
+      url: queryURL2,
+      method: "GET"
+  }).then(function(response){
+      for(i=0;i<5;i++){
+          $("#song-" + i).text((i+1) +". "+response.toptracks.track[i].name)
+      }
+  })
 }
 
-function ticketSearch(artistSearch){
-  var queryURL1 = "https://rest.bandsintown.com/artists/" + artistSearch + "/?app_id=45255e6cc480b6c589613047fb0d5749"
-
-  
+function BIT(artist){
+  var queryURL1 = "https://rest.bandsintown.com/artists/" + artist + "/?app_id=45255e6cc480b6c589613047fb0d5749"
   $.ajax({
-    url: queryURL1,
-    method: "GET",
+      url: queryURL1,
+      method: "GET",
   }).then(function(response){
-    //Response from API
-    console.log(response)
+      console.log(response)
 
-    //Number of Events Coming Up
-    console.log(response.upcoming_event_count)
-    events = response.upcoming_event_count
+      //Artist Image
+      $("#artist-image").attr("src", response.image_url)
 
-    //Artist Image
-    $("#artistImg").attr("src", response.image_url)
+      //Gets the Number of Events
+      
+  })
+}
+
+function createCard(){
+      //ADD BUTTON
+      var button = $("<button>");
+      button.addClass("btn-floating waves-effect waves-light red right");
+      button.css({
+          "margin-right": "10px",
+          "margin-top": "10px",
+      });
+      var symbol = $("<i>");
+      symbol.addClass("material-icons").text("add")
+      button.append(symbol)
+      
+      var newcol = $("<div>");
+      newcol.addClass("col s12 m6");
+      
+      var newcard = $("<div>");
+      newcard.addClass("card blue-grey darken-1");
   
-
-  var queryURL2 = "https://rest.bandsintown.com/artists/" + artistSearch + "/events/?app_id=45255e6cc480b6c589613047fb0d5749"
-
-  $.ajax({
-    url: queryURL2,
-    method: "GET",
-  }).then(function(response){
-    console.log(events)
-    
-    if(events > 0){
-      for(i = 0; i < 3; i++){
-        let date = new Date( Date.parse(response[i].datetime))
-        $("#time" + i).text(date)
-        $("#location" + i).text(response[i].venue.location)
-      }
-    }else{
-        for(i = 0; i < 3; i++){
-          $("#time" + i).text("No Upcoming Events")
-          $("#location" + i).text("")
-      }
-    }
-
-    // let date0 = new Date( Date.parse(response[0].datetime))
-    // $("#time0").text(date0);
-    // $("#location0").text(response[0].venue.location);
-
-    // let date1 = new Date( Date.parse(response[1].datetime))
-    // $("#time1").text(date1);
-    // $("#location1").text(response[1].venue.location);
-
-    // let date2 = new Date( Date.parse(response[2].datetime))
-    // $("#time2").text(date2);
-    // $("#location2").text(response[2].venue.location);
-})})
+      var cardimage = $("<div>");
+      cardimage.addClass("card-image");
+      cardimage.append(button);
+  
+      var cardbody = $("<div>");
+      cardbody.addClass("card-content white-text");
+  
+      var cardtitle = $("<span>");
+      cardtitle.addClass("card-title");
+      cardtitle.text("This is created from JS");
+  
+      var cardinformation = $("<p>");
+      cardinformation.text("This information came from JS");
+  
+      cardbody.append(cardtitle);
+      cardbody.append(cardinformation);
+      newcard.append(cardimage);
+      newcard.append(cardbody);
+      newcol.append(newcard);
+  
+      $("#events-area").append(newcol);
 }
