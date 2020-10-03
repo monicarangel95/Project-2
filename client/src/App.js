b8a16e9053e43ed3dd5b62bc454f1de5390b055e
Monica Rangel <monicarangel95@github.com>
2020-10-03 11:09:35 -0700

    Update app.js to set up grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import Navbar from "./components/layout/Navbar.js";
+import Navbar from "./components/layout/Navbar/Navbar.js";
 import Search from "./components/layout/Search/Search.js";
-
+import Grid from "./components/layout/Grid/Grid.js";
 function App() {
   return (
 <div>
 <Navbar />
 <Search />
+<Grid />
 </div>
   );
 }
