f713d29d300d972fa9c730f6fce2312b66f31a4e
Monica Rangel <monicarangel95@github.com>
2020-10-10 10:25:02 -0700

    Update Home.js to include LastFM API call

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Search from "../components/layout/Search/Search.js";
 import Grid from "../components/layout/Grid/Grid.js";
 import Header from "../components/layout/Header/Header.js";
 import ArtistInfo from "../components/layout/ArtistInfo/ArtistInfo.js";
 // import EventCard from "../components/layout/EventCard/EventCard.js";
-// import API from "../utils/API.js"
+import API from "../utils/API.js"
 
 function Home() {
     const [artist, setArtist] = useState("")
@@ -19,25 +19,24 @@ function Home() {
     const handleFormSubmit = (event) => {
         event.preventDefault();
         console.log(artist);
-        // Make api calls here, update state, pass state and pass object to setArtistData
-        // Update that object to have data
-    } 
-
-
+        API.fetchArtist(artist) .then( data => {
+            // console.log (data.data)
+            setArtistData({...artistData, ...data.data})
+        } )
+    }
+    
     // Make API call here with artist name from Search.js(stored as state and pass state here)
     // Somehow, convert function createCard() into component?
+    console.log(artistData)
     return (
         <div>
             <Header>
                 <Search handleFormSubmit={handleFormSubmit}
-                handleInputChange={handleInputChange} />
+                    handleInputChange={handleInputChange} />
             </Header>
-            <Grid />
-            <ArtistInfo />
-                {/* Pass artistData as props into ArtistInfo*/}
-                {/* If (!artistData) ? show Grid : hide Grid/show ArtistInfo */}
-            {/* <EventCard /> */}
-                {/* If (artistinfo && eventinfo) exists ? hide Grid : else show Grid */}
+            {!artistData.artist ? <Grid /> : <ArtistInfo  />}
+            {/* {!artistData.artist ? <Grid /> : <EventCard/>} */}
+
         </div>
     )
 }
