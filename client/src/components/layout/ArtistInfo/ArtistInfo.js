5c3f5c9d6ba70f15c9d064c53dd29832093b31f5
Monica Rangel <monicarangel95@github.com>
2020-10-10 10:25:28 -0700

    Update to Artist Info to remove "hide" class

--- client/src/components/layout/ArtistInfo/ArtistInfo.js
+++ client/src/components/layout/ArtistInfo/ArtistInfo.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 import "./ArtistInfo.css"; 
 
 function ArtistInfo() {
     // render() {
     return (
-        <div id="artist" class="hide">
+        <div id="artist" class="">
+            {/* ARTIST CARD */}
             <div class="container z-depth-1" style={{ marginTop: "50px"}}>
                 <div class="row">
+                    {/* ARTIST NAME and PICTURE */}
                     <div class="col s12 l2 center" >
-                        <img 
-                            id="artist-image" 
-                            class="z-depth-2"
-                            alt="Picture of Searched Artist"
-                            src="" style={{ marginTop: "20px", maxWidth: "150px"}} 
-                        />
-                        <h2 id="artist-name" style={{ marginTop: "0px"}} />
+                        <img id="artist-image" class="z-depth-2"alt="Picture of Searched Artist"src="" style={{ marginTop: "20px", maxWidth: "150px"}} />
+                    {/* ARTIST NAME */}
+                    <h2 id="artist-name" style={{ marginTop: "0px"}} />
                         <p style={{ marginTop: "-15px" }} >
                             <i id="genre"></i>
                         </p>
                     </div>
+                    {/* ARTIST BIO & SONGS */}
                     <div class="col s12 l10" >
+                        {/* BIO */}
                         <h6><strong>Biography</strong></h6>
                         <p id="summary"></p>
                     </div>
+                    {/* TOP SONGS */}
                     <div class="col s12 l6">
                         <h6><strong>Top Songs</strong></h6>
                         <ul id="top-songs" class="collection">
