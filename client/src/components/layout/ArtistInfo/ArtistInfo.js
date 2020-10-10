94d24219916e41d01b0abd279269eb1040cc23e8
Andrew <andrewyen64@gmail.com>
2020-10-10 14:23:22 -0700

    Add name, bio, genre, song1-5 props (passed in from Home page)

--- client/src/components/layout/ArtistInfo/ArtistInfo.js
+++ client/src/components/layout/ArtistInfo/ArtistInfo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./ArtistInfo.css"; 
 
-function ArtistInfo() {
+function ArtistInfo( {name, bio, genre, song1, song2, song3, song4, song5 } ) {
     // render() {
     return (
         <div id="artist" class="">
@@ -10,28 +10,32 @@ function ArtistInfo() {
                 <div class="row">
                     {/* ARTIST NAME and PICTURE */}
                     <div class="col s12 l2 center" >
-                        <img id="artist-image" class="z-depth-2"alt="Picture of Searched Artist"src="" style={{ marginTop: "20px", maxWidth: "150px"}} />
+                        <img id="artist-image" class="z-depth-2"alt="Searched Artist"src="" style={{ marginTop: "20px", maxWidth: "150px"}} />
                     {/* ARTIST NAME */}
-                    <h2 id="artist-name" style={{ marginTop: "0px"}} />
+                    <h2 id="artist-name" style={{ marginTop: "0px"}} >
+                        {name}
+                    </h2>
                         <p style={{ marginTop: "-15px" }} >
-                            <i id="genre"></i>
+                            <i id="genre">{genre}</i>
                         </p>
                     </div>
                     {/* ARTIST BIO & SONGS */}
                     <div class="col s12 l10" >
                         {/* BIO */}
                         <h6><strong>Biography</strong></h6>
-                        <p id="summary"></p>
+                        <p id="summary">
+                            {bio}
+                        </p>
                     </div>
                     {/* TOP SONGS */}
                     <div class="col s12 l6">
                         <h6><strong>Top Songs</strong></h6>
                         <ul id="top-songs" class="collection">
-                            <li id="song-0" class="collection-item">Song 0</li>
-                            <li id="song-1" class="collection-item">Song 1</li>
-                            <li id="song-2" class="collection-item">Song 2</li>
-                            <li id="song-3" class="collection-item">Song 3</li>
-                            <li id="song-4" class="collection-item">Song 4</li>
+                            <li id="song-0" class="collection-item">{song1}</li>
+                            <li id="song-1" class="collection-item">{song2}</li>
+                            <li id="song-2" class="collection-item">{song3}</li>
+                            <li id="song-3" class="collection-item">{song4}</li>
+                            <li id="song-4" class="collection-item">{song5}</li>
                         </ul>
                     </div>
                 </div>
