867a0c60a5bbe16404a2dd888bfb2703e2cc748e
Andrew <andrewyen64@gmail.com>
2020-10-13 00:08:12 -0700

    Adjusted searchbar size class and removed unused comments

--- client/src/components/layout/Search/Search.js
+++ client/src/components/layout/Search/Search.js
@@ -1,48 +1,32 @@
-import React, { useState } from 'react'
+import React from 'react'
 import "./Search.css"; 
 
 const Search = (props) => {
-    // state = {}       **[a variable to store data in]**
-    // setState that includes object with artist name in search
-    // const [artist, setArtist] = useState("")
-
-    // function handleSubmit()
-    // const handleSubmit = e => {
-    //     e.preventDefault();
-    //     console.log("Artist's name is '" + artist + "'");
-    // };
-
-    // render() {
-        return (
-            <div>
-                <div className="center">
-                    <div id="search-area" class="col l7 s12 offset-l2" >
-                        <form action="#">
-                            <div class="input-field">
-                                <input 
-                                    id="search-artist"
-                                    type="text" 
-                                    class="materialize-textarea"
-                                    onChange={props.handleInputChange}
-                                    />
-                                <label for="search-artist">Search Artist</label>
-                            </div>
-                        </form>
+    return (
+        <div className="center">
+            <div id="search-area" class="col l5 s12 offset-l3" >
+                <form action="#">
+                    <div class="input-field">
+                        <input 
+                            id="search-artist"
+                            type="text" 
+                            class="materialize-textarea"
+                            onChange={props.handleInputChange}
+                            />
+                        <label for="search-artist">Search Artist</label>
                     </div>
-                    <div class="col l2 s12" style= {{paddingTop: "23px"}}>
-                        <div
-                            id="search-button"
-                            class="btn waves-effect waves-light"
-                            onClick={props.handleFormSubmit}
-                            type="submit"
-                            > Search
-                        </div>
-                        {/* // replace ^^ with onclick = function handleSubmit()
-                    // conditional rendering: if artist-info exists, populate; else don't */}
-                    </div>
+                </form>
+            </div>
+            <div class="col l2 s12" style= {{paddingTop: "15px"}}>
+                <div
+                    id="search-button"
+                    class="btn waves-effect waves-light"
+                    onClick={props.handleFormSubmit}
+                    type="submit"
+                    > Search
                 </div>
             </div>
-        )
-    // }
+        </div>
+    )
 }
 export default Search
