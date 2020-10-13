c2a23327714cb348547fa48ada06ca503c014895
Andrew <andrewyen64@gmail.com>
2020-10-12 23:50:50 -0700

    Adjusted and Added responsive classes to Grid

--- client/src/components/layout/Grid/Grid.js
+++ client/src/components/layout/Grid/Grid.js
@@ -10,17 +10,17 @@ function Grid() {
             <section id="photos" class="container section" style={{ marginTop: "30px" }}>
                   {/*  Search your favorite artist */}
                 <div class="row">
-                    <div class="col s12 l4 push-l2">
+                    <div class="col s12 l4 push-l1 xl4 push-xl1">
                         <img src={artist} alt="Artist on Stage" class="responsive-img z-depth-2" />
                     </div>
-                    <div class="col s12 m6 l6 offset-l1">
+                    <div class="col s12 m6 l6 xl 3 offset-l1">
                         <h3 class="indigo-text text-darken 4">Search Your Favorite Artists</h3>
                         <p>Search for your favorite artists to find out their albums, top songs, and upcoming events around the world.</p>
                     </div>
                 </div>
                 {/* Look for upcoming events */}
                 <div class="row">
-                    <div class="col s12 l4 push-l7">
+                    <div class="col s12 l4 push-l7 xl4 push-xl7">
                         <img src={events} alt="Concert Event" class="responsive-img z-depth-2" />
                     </div>
                     <div class="col s12 l6 pull-l4 offset-l1">
@@ -30,7 +30,7 @@ function Grid() {
                 </div>
                 {/* Find An Artist's Top Songs */}
                 <div class="row">
-                    <div class="col s12 l4 push-l2">
+                    <div class="col s12 l4 push-l1 xl4 push-xl1">
                         <img src={music} alt="Artist Playing Songs" class="responsive-img z-depth-2" />
                     </div>
                     <div class="col s12 l6 offset-l1">
