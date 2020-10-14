8dc3faffdeb4560bb3bbd734f9198d6a54839e49
Andrew <andrewyen64@gmail.com>
2020-10-13 20:06:07 -0700

    Remove unused code/comments and change class -> className in EventCard

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,10 +1,11 @@
 import React from 'react';
 // import Search, { Navbar } from "../components/layout/Navbar/Navbar.js";
 import Header from "../components/layout/Header/Header.js";
-import Row from "../components/layout/Row/Row.js";
+// import Row from "../components/layout/Row/Row.js";
 import EventCard from "../components/layout/EventCard/EventCard.js";
 
 function Events() {
+  
   return (
     <div>
       {/* <Navbar/> */}
