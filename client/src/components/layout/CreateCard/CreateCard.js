86e8c7b09689c4223211a9517e8c9edd0b58fa2d
Monica Rangel <monicarangel95@github.com>
2020-10-16 22:01:57 -0700

    adding code from the event card and fixing the wide formating

--- client/src/components/layout/CreateCard/CreateCard.js
+++ client/src/components/layout/CreateCard/CreateCard.js
@@ -4,22 +4,28 @@ import "./CreateCard.css"
 function CreateCard({ location, date }) {
     // function createCard(date, location, number) **pass in with props
 
-    return(
-        <div class="col s12 m6">
-            <div class="card blue-grey darken-1">
-                <div class="card-image">
-                    <button class="btn-floating waves-effect waves-light red right" id="0" 
-                        onclick="savethis(this.id)" 
-                        style={{marginRight: "10px", marginTop: "10px"}}>
-                        <i class="material-icons">add</i>
-                    </button>
-                </div>
-                <div class="card-content white-text">
-                    <span class="card-title" id="location-0">{location}</span>
-                        <p id="date-0">{date}</p>
+    return (
+        <section id="events-searched">
+            <div className="container">
+                <div id="events-area" className="row">
+                    <div className="col s12 m6">
+                        <div className="card blue-grey darken-1">
+                            <div className="card-image">
+                                <button className="btn-floating waves-effect waves-light red right" id="0"
+                                    onClick="savethis(this.id)"
+                                    style={{ marginRight: "10px", marginTop: "10px" }}>
+                                    <i className="material-icons">add</i>
+                                </button>
+                            </div>
+                                <div className="card-content white-text">
+                                <span className="card-title" id="location-0">{location}</span>
+                                    <p id="date-0">{date}</p>
+                                </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
